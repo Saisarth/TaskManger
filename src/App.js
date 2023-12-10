35c6da1826a50b1fb861a0d8d542cc0a98d36db8
Saisarth <119151619+Saisarth@users.noreply.github.com>
2023-12-10 11:55:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <h1 className="text-4xl font-bold mb-4">Task Manager</h1>
+      <h1 className="text-4xl font-bold mb-4">Task todo</h1>
 
       <TaskForm
         addTask={addTask}
